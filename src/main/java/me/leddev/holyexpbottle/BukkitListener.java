aad0d9ab276fef9baae9510ae90a724b77ee5f96
ConderFix <117036518+KonderFix@users.noreply.github.com>
2023-09-08 16:21:25 +0300

    Фикс дюпа: Если бутылкой с опытом открыть сундук, то бутылка останется и опыт выдаст

--- src/main/java/me/leddev/holyexpbottle/BukkitListener.java
+++ src/main/java/me/leddev/holyexpbottle/BukkitListener.java
@@ -6,13 +6,16 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BukkitListener implements Listener {
 
@@ -40,6 +43,27 @@ public class BukkitListener implements Listener {
                             PersistentDataType.STRING);
                     if (value != null && value.equals("one")) {
                         player.giveExp(plugin.getConfig().getInt("item_bottle_one.value"));
+                        ItemStack itemStack = new ItemStack(Material.EXPERIENCE_BOTTLE, 1);
+                        ItemMeta itemMeta2 = itemStack.getItemMeta();
+                        itemMeta2.setDisplayName(HexUtil.translate(Main.getInstance().getConfig().getString("item_bottle_one.name")));
+                        itemMeta2.addEnchant(Enchantment.OXYGEN, 1, true);
+                        itemMeta2.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+
+                        itemMeta2.getPersistentDataContainer().set(
+                                NamespacedKey.fromString("bottle_one"),
+                                PersistentDataType.STRING, "one"
+                        );
+
+                        List<String> lore = Main.getInstance().getConfig().getStringList("item_bottle_one.lore");
+                        List<String> translatedLore = new ArrayList<>();
+                        for (String line : lore) {
+                            String translatedLine = HexUtil.translate(line);
+                            translatedLore.add(translatedLine);
+                        }
+                        itemMeta2.setLore(translatedLore);
+
+                        itemStack.setItemMeta(itemMeta2);
+                        player.getInventory().removeItem(itemStack);
                     }
                 }
             }
@@ -64,6 +88,27 @@ public class BukkitListener implements Listener {
                             PersistentDataType.STRING);
                     if (value != null && value.equals("two")) {
                         player.giveExp(plugin.getConfig().getInt("item_bottle_two.value"));
+                        ItemStack itemStack = new ItemStack(Material.EXPERIENCE_BOTTLE, 1);
+                        ItemMeta itemMeta2 = itemStack.getItemMeta();
+                        itemMeta2.setDisplayName(HexUtil.translate(Main.getInstance().getConfig().getString("item_bottle_two.name")));
+                        itemMeta2.addEnchant(Enchantment.OXYGEN, 1, true);
+                        itemMeta2.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+
+                        itemMeta2.getPersistentDataContainer().set(
+                                NamespacedKey.fromString("bottle_two"),
+                                PersistentDataType.STRING, "two"
+                        );
+
+                        List<String> lore = Main.getInstance().getConfig().getStringList("item_bottle_two.lore");
+                        List<String> translatedLore = new ArrayList<>();
+                        for (String line : lore) {
+                            String translatedLine = HexUtil.translate(line);
+                            translatedLore.add(translatedLine);
+                        }
+                        itemMeta2.setLore(translatedLore);
+
+                        itemStack.setItemMeta(itemMeta2);
+                        player.getInventory().removeItem(itemStack);
                     }
                 }
             }
@@ -88,6 +133,27 @@ public class BukkitListener implements Listener {
                             PersistentDataType.STRING);
                     if (value != null && value.equals("three")) {
                         player.giveExp(plugin.getConfig().getInt("item_bottle_three.value"));
+                        ItemStack itemStack = new ItemStack(Material.EXPERIENCE_BOTTLE, 1);
+                        ItemMeta itemMeta2 = itemStack.getItemMeta();
+                        itemMeta2.setDisplayName(HexUtil.translate(Main.getInstance().getConfig().getString("item_bottle_three.name")));
+                        itemMeta2.addEnchant(Enchantment.OXYGEN, 1, true);
+                        itemMeta2.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+
+                        itemMeta2.getPersistentDataContainer().set(
+                                NamespacedKey.fromString("bottle_three"),
+                                PersistentDataType.STRING, "three"
+                        );
+
+                        List<String> lore = Main.getInstance().getConfig().getStringList("item_bottle_three.lore");
+                        List<String> translatedLore = new ArrayList<>();
+                        for (String line : lore) {
+                            String translatedLine = HexUtil.translate(line);
+                            translatedLore.add(translatedLine);
+                        }
+                        itemMeta2.setLore(translatedLore);
+
+                        itemStack.setItemMeta(itemMeta2);
+                        player.getInventory().removeItem(itemStack);
                     }
                 }
             }
